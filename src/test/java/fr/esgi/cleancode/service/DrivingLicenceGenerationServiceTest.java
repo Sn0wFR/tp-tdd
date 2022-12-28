8133452a85921c70db8747c5763a7518e01d5f0b
hemarydem <hamedremy@hotmail.fr>
2022-12-28 19:34:28 +0100

    init DrivingLicenceGenerationServiceTest.java

---
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceGenerationServiceTest.java
@@ -0,0 +1,46 @@
+package fr.esgi.cleancode.service;
+
+import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+public class DrivingLicenceGenerationServiceTest {
+
+    @Mock
+    InMemoryDatabase database;
+
+    @InjectMocks
+    DrivingLicenceGenerationService drivingLicenceGenerationService;
+
+    @Test
+    void should_generate_driving_licence() {
+        final var drivingLicence = drivingLicenceGenerationService.generateNewDrivingLicence("123456789012345");
+        assertThat(drivingLicence.getId())
+                .isNotNull();
+        assertThat(drivingLicence.getAvailablePoints())
+                .isNotNull()
+                .isEqualTo(12);
+        assertThat(drivingLicence.getDriverSocialSecurityNumber())
+                .isNotNull();
+
+        when(database.findById(drivingLicence.getId())).thenReturn(Optional.of(drivingLicence));
+
+        assertThat(database.findById(drivingLicence.getId()).isPresent())
+                .isTrue();
+
+        assertThat(drivingLicence).isInstanceOf(DrivingLicence.class);
+    }
+}
