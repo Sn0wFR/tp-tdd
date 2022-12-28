f55e28e9be82ac5b9486968519bfee63b64d23e0
hemarydem <hamedremy@hotmail.fr>
2022-12-28 20:53:24 +0100

    init  DrivingLicenceGenerationService.java

---
+++ src/main/java/fr/esgi/cleancode/service/DrivingLicenceGenerationService.java
@@ -0,0 +1,33 @@
+package fr.esgi.cleancode.service;
+
+import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
+import fr.esgi.cleancode.model.DrivingLicence;
+
+import java.util.UUID;
+
+public class DrivingLicenceGenerationService {
+
+    private final SocialSecurityNumberValidatorService socialSecurityNumberValidatorService  = new SocialSecurityNumberValidatorService();
+
+    private final DrivingLicenceIdGenerationService drivingLicenceIdGenerationService  = new DrivingLicenceIdGenerationService();
+
+    public DrivingLicence generateNewDrivingLicence(String driverSocialSecurityNumber) {
+
+        if(!this.socialSecurityNumberValidatorService.isNotNull(driverSocialSecurityNumber))
+            throw new InvalidDriverSocialSecurityNumberException("Social security number is null");
+
+        if(!this.socialSecurityNumberValidatorService.isEqualsTo15Numbers(driverSocialSecurityNumber))
+            throw new InvalidDriverSocialSecurityNumberException("Social security number is not equals to 15 numbers");
+
+        if(!this.socialSecurityNumberValidatorService.isOnlyNumbers(driverSocialSecurityNumber))
+            throw new InvalidDriverSocialSecurityNumberException("Social security number is not only numbers");
+
+        DrivingLicence drivingLicence = DrivingLicence.builder()
+                .id(drivingLicenceIdGenerationService.generateNewDrivingLicenceId())
+                .driverSocialSecurityNumber(driverSocialSecurityNumber)
+                .build();
+
+        return InMemoryDatabase.getInstance().save(drivingLicence.getId(), drivingLicence);
+    }
+}
